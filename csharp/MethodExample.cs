e347464aab8d4eba7166afec08bb0ca70b69e9fa
Gianna <142685276+Giannasaurus@users.noreply.github.com>
2025-06-05 16:40:35 +0800

    Create MethodExample.cs

---
+++ csharp/MethodExample.cs
@@ -0,0 +1,11 @@
+using System;
+
+class MethodExample {
+    static void Greet(string name) {
+        Console.WriteLine($"Hello, {name}!");
+    }
+
+    static void Main() {
+        Greet("Gianna");
+    }
+}
